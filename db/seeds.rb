498d79e51c33d4b18c404462e3651aa5c9661ced
ptalk <pierre.lekhac@sciencespo.fr>
2018-08-23 17:07:25 +0200

    basic setup model controller db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  Email.create!(object: "JaRule", body: Faker::Internet.email)
+end
---
+++ test/controllers/email_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EmailControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get email_index_url
+    assert_response :success
+end
